b7bad3b806c3c43f5d772686daf41b9325ccd3cf
neimad <dt2g23@soton.ac.uk>
2024-07-08 01:30:24 +0100

    Lonely Integer completed

---
+++ 3 Month Preparation Kit/Week 2/Lonely Integer/Solution.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'lonelyinteger' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int lonelyinteger(List<Integer> a) {
+    // Write your code here
+        Set<Integer> lonely = new HashSet<>(); //Create HashSet to use .contains() method
+        for (int i : a) { //Loops through all elements in array a
+            if (lonely.contains(i)) { //If seen array has the integer then
+                lonely.remove(i); //Remove the integer as it is not lonely
+            } else {
+                lonely.add(i); //Else add integer to lonely array
+            }
+        }
+        if (lonely.size() > 0) { //If there is a lonely integer
+            return lonely.iterator().next(); //Return first element
+        } else {
+            return 0; //Else return no lonely integer
+        }
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.lonelyinteger(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
